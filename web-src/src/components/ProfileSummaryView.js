92a1aaf3a5eedfff4b433d1b88d97e9de5a1b297
Pulkit Chadha <pulkitchadha@Pulkits-MacBook-Pro.local>
2021-03-25 07:36:13 -0400

    fixed null identitymap error

--- web-src/src/components/ProfileSummaryView.js
+++ web-src/src/components/ProfileSummaryView.js
@@ -11,6 +11,8 @@ const ProfileSummaryView = (props) => {
   const personName = person.name || {};
   const firstName = personName.firstName || "";
   const lastName = personName.lastName || "";
+  const identityMap = props.profileData.identityMap || {};
+
   return (
     <View
       backgroundColor="gray-75"
@@ -28,7 +30,7 @@ const ProfileSummaryView = (props) => {
         <TestProfile alignSelf="center" size="XL" />
         <Heading level={4}>{`${firstName} ${lastName}`}</Heading>
         <Header level={3}>Identities</Header>
-        {Object.keys(props.profileData.identityMap).map((namespace) =>
+        {Object.keys(identityMap).map((namespace) =>
           props.profileData.identityMap[namespace].map((identity) => (
             <Link
               key={`${namespace}-${identity.id}`}
